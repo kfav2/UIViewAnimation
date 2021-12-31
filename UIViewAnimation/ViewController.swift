527ed84b8fd07817952f841ae1e0917f5a681762
KFAV <kofava14@gmail.com>
2021-12-31 11:03:08 -0800

    Ch. 4 2 UIViewAnimate Part 1: User Interface Set Up

--- UIViewAnimation/ViewController.swift
+++ UIViewAnimation/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var moveButton: UIButton!
+    @IBOutlet weak var alphaButton: UIButton!
+    @IBOutlet weak var pulseButton: UIButton!
+    @IBOutlet weak var colorChangeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func moveButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func alphaButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func pulseButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func colorChangeButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 
 }
 
